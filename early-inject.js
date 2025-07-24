cc0b3825787fc7662c67b3f04ded73c6cc2b8fb4
augustkimo <august.kimo@icloud.com>
2025-07-24 12:43:21 -0400

    typo

--- early-inject.js
+++ early-inject.js
@@ -1,5 +1,5 @@
 (function () {
-  const defaultSpeeds = [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 5, 7.5, 10, 13, 15];
+  const defaultSpeeds = [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 5, 7.5, 10, 13, 16];
   let userSpeeds = defaultSpeeds;
   try {
       const stored = window.localStorage.getItem('udemyChangerSpeeds');
